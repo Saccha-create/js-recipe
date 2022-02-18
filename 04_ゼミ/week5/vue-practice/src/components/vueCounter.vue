b64e7bd24f2ecf9b860639a9308c1128ef83f536
fukushima_satsuki <fukushima.satsuki@kindai.ac.jp>
2022-02-18 16:36:03 +0900

    vueの復讐をやったよ

---
+++ 04_ゼミ/week5/vue-practice/src/components/vueCounter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>🔥 {{ number }} 🔥</div>
+  <button v-on:click="countUp">+1</button>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    }
+  },
+  created: function () {
+    this.count = 100
+  },
+  computed: {
+    number: function () {
+      if (this.count % 3 === 0) {
+        return this.count + "!!!!"
+      } else {
+        return this.count
+      }
+    },
+  },
+
+  methods: {
+    countUp: function () {
+      this.count += 1
+    },
+  },
+}
+</script>
